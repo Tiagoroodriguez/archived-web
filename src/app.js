a27accc99e751b2ce061b4f492659491bd7534e5
Tiago Rodriguez <110980693+Tiagoroodriguez@users.noreply.github.com>
2024-04-14 13:41:51 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://archived-q8l1sawft-tiagoroodriguezs-projects.vercel.app/',
+    origin: 'https://archived-q8l1sawft-tiagoroodriguezs-projects.vercel.app',
     credentials: true,
   })
 );
