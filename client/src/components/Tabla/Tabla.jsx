2632fc3216788b8edce00e6bc77b1a9993f12aa7
Tiago <tiagorodriguez0202@gmail.com>
2024-05-01 16:29:11 -0300

    guia de talles

--- client/src/components/Tabla/Tabla.jsx
+++ client/src/components/Tabla/Tabla.jsx
@@ -10,41 +10,50 @@ export function Tabla({
   lLargo,
   xlAncho,
   xlLargo,
+  onClose,
 }) {
   return (
-    <>
-      <span className='titulo'>{`${producto}`}</span>
-      <table className='tabla-container'>
-        <thead>
-          <tr className='tabla-fila'>
-            <td>Talle</td>
-            <td>Ancho</td>
-            <td>Largo</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='tabla-fila'>
-            <td>S</td>
-            <td>{`${sAncho}`}</td>
-            <td>{`${sLargo}`}</td>
-          </tr>
-          <tr className='tabla-fila'>
-            <td>M</td>
-            <td>{`${mAncho}`}</td>
-            <td>{`${mLargo}`}</td>
-          </tr>
-          <tr className='tabla-fila'>
-            <td>L</td>
-            <td>{`${lAncho}`}</td>
-            <td>{`${lLargo}`}</td>
-          </tr>
-          <tr className='tabla-fila'>
-            <td>XL</td>
-            <td>{`${xlAncho}`}</td>
-            <td>{`${xlLargo}`}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <div className='modal'>
+      <div className='modal-contenido'>
+        <span className='titulo'>{producto}</span>
+        <table className='tabla-container'>
+          <thead>
+            <tr className='tabla-fila'>
+              <td>Talle</td>
+              <td>Ancho</td>
+              <td>Largo</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className='tabla-fila'>
+              <td>S</td>
+              <td>{sAncho}</td>
+              <td>{sLargo}</td>
+            </tr>
+            <tr className='tabla-fila'>
+              <td>M</td>
+              <td>{mAncho}</td>
+              <td>{mLargo}</td>
+            </tr>
+            <tr className='tabla-fila'>
+              <td>L</td>
+              <td>{lAncho}</td>
+              <td>{lLargo}</td>
+            </tr>
+            <tr className='tabla-fila'>
+              <td>XL</td>
+              <td>{xlAncho}</td>
+              <td>{xlLargo}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button
+          className='table-button'
+          onClick={onClose}
+        >
+          <i className='bi bi-x-lg' />
+        </button>
+      </div>
+    </div>
   );
 }
