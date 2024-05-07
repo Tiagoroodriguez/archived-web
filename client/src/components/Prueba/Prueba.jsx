928577217f4e7aed737e59324356fb9c61f540fe
Tiago <tiagorodriguez0202@gmail.com>
2024-05-07 13:30:31 -0300

    fixeds

---
+++ client/src/components/Prueba/Prueba.jsx
@@ -0,0 +1,12 @@
+import './Prueba.css';
+
+export default function Prueba() {
+  return (
+    <>
+      <div className='input-container'>
+        <input className='input-1' />
+        <label className='labe-1'>Contraseña</label>
+      </div>
+    </>
+  );
+}
