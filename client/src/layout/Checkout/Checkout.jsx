ad8750928e494a0bc044a75db45e68c948154cf6
Tiago <tiagorodriguez0202@gmail.com>
2024-05-06 21:42:26 -0300

    checkout layout

--- client/src/layout/Checkout/Checkout.jsx
+++ client/src/layout/Checkout/Checkout.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CarritoContext';
 import Input from '../../components/Input/Input';
 import { Boton } from '../../components/Boton/Boton';
@@ -13,7 +14,7 @@ export default function Checkout() {
     <>
       <main className='checkout'>
         <section className='informacion-section'>
-          <div className='superior'>
+          <header className='superior'>
             <LogoTexto />
             <ul>
               <li className='point'>
@@ -27,6 +28,15 @@ export default function Checkout() {
               <li className='line'></li>
               <li className='point'>
                 <button className='focus'>
+                  <div>
+                    <i className='bi bi-info-lg'></i>
+                  </div>
+                  <p>Informacion</p>
+                </button>
+              </li>
+              <li className='line'></li>
+              <li className='point'>
+                <button>
                   <div>
                     <i className='bi bi-box-seam'></i>
                   </div>
@@ -43,61 +53,75 @@ export default function Checkout() {
                 </button>
               </li>
             </ul>
-          </div>
+          </header>
+          <form className='form-datos-envio'>
+            <div className='informacion-contacto'>
+              <p>Información de contacto</p>
+              <Input
+                type='email'
+                placeholder='ejemplo@gmail.com'
+                label='Correo electrónico'
+              />
+            </div>
 
-          <div className='informacion-contacto'>
-            <p>Información de contacto</p>
-            <Input
-              type='email'
-              placeholder='ejemplo@gmail.com'
-              label='Correo electrónico'
-            />
-          </div>
+            <div className='informacion-envio'>
+              <p>Dirección de envio</p>
+              <Input
+                type='text'
+                placeholder='nombre'
+                label='Nombre'
+              />
+              <Input
+                type='text'
+                placeholder='apellido'
+                label='Apellido'
+              />
+              <Input
+                type='number'
+                placeholder='0000000000'
+                label='Telefono'
+              />
+              <Input
+                type='text'
+                placeholder='dirección'
+                label='Dirección'
+              />
+              <div className='ciudad-container'>
+                <div className='ciudad-item ciudad'>
+                  <Input
+                    type='text'
+                    placeholder='ciudad'
+                    label='Ciudad'
+                  />
+                </div>
 
-          <div className='informacion-envio'>
-            <p>Dirección de envio</p>
-            <Input
-              type='text'
-              placeholder='nombre'
-              label='Nombre'
-            />
-            <Input
-              type='text'
-              placeholder='apellido'
-              label='Apellido'
-            />
-            <Input
-              type='number'
-              placeholder='0000000000'
-              label='Telefono'
-            />
-            <Input
-              type='text'
-              placeholder='dirección'
-              label='Dirección'
-            />
-            <Input
-              type='number'
-              placeholder='XXXXX'
-              label='Codigo postal'
-            />
-            <Input
-              type='text'
-              placeholder='ciudad'
-              label='Ciudad'
-            />
-          </div>
+                <div className='ciudad-item'>
+                  <Input
+                    type='number'
+                    placeholder='XXXXX'
+                    label='Codigo postal'
+                  />
+                </div>
+              </div>
+            </div>
 
-          <div className='checkout-actions'>
-            <Boton
-              textBoton='Volver'
-              secundario={true}
-            />
-            <Boton textBoton='Continuar' />
-          </div>
+            <div className='checkout-actions'>
+              <Link to='/carrito'>
+                <Boton
+                  textBoton='Volver'
+                  secundario={true}
+                />
+              </Link>
+              <Boton textBoton='Continuar' />
+            </div>
+          </form>
         </section>
 
         <section className='carrito-section'>
+          <header className='header-carrito'>
+            <h1>Detalle de compra</h1>
+            <div className='line-costo' />
+          </header>
           <div className='cart-item-container cart-item-container-checkout'>
             {cartItems.map((item) => (
               <div
@@ -113,20 +137,23 @@ export default function Checkout() {
                   </div>
                   <div className='info-producto checkout-info-cart'>
                     <h1 className=''>{item.nombre}</h1>
-                    <p className=''>{`$${item.precio}`}</p>
+                    <p>{`Talle: ${item.talle}`}</p>
+                    <p>{`Cantidad: ${item.quantity}`}</p>
+                    <p className=''>{`$${item.precio * item.quantity}`}</p>
                   </div>
                 </div>
               </div>
             ))}
           </div>
           <div className='descripcion-final-checkout'>
+            <div className='line-costo' />
             <div>
               <p>Subtotal</p>
               <p>${getCartTotal()}</p>
             </div>
             <div>
               <p>Costo de envio</p>
-              <p>${getCartTotal()}</p>
+              <p>Calculado en el siguiente paso</p>
             </div>
             <div>
               <h1>Total</h1>
