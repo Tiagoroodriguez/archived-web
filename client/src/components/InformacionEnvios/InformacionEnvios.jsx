66a1d3595b8bb61258fcf205a509fa297147b10f
Tiago <tiagorodriguez0202@gmail.com>
2024-05-13 21:13:09 -0300

    Update Carrito and Header components styling and layout

--- client/src/components/InformacionEnvios/InformacionEnvios.jsx
+++ client/src/components/InformacionEnvios/InformacionEnvios.jsx
@@ -1,26 +1,26 @@
 import './InformacionEnvios.css';
 
 export function InformacionEnvios() {
-    return(
-        <>
-        <section>
-            <div className="info-container">
-                <div className="info celu">
-                    <i className="bi bi-box-seam"></i>
-                    <p>Envios a todo el país</p>
-                </div>
-                <div className="linea"></div>
-                <div className="info">
-                    <i className="bi bi-credit-card"></i>
-                    <p>Paga con tarjeta de debito o credito</p>
-                </div>
-                <div className="linea"></div>
-                <div className="info">
-                    <i className="bi bi-lock"></i>
-                    <p>Compra con seguridad</p>
-                </div>
-            </div>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section>
+        <div className='info-container'>
+          <div className='info celu'>
+            <i className='bi bi-box-seam'></i>
+            <p>Envios a todo el país</p>
+          </div>
+          <div className='linea'></div>
+          <div className='info'>
+            <i className='bi bi-credit-card'></i>
+            <p>Paga con tarjeta de debito o credito</p>
+          </div>
+          <div className='linea'></div>
+          <div className='info'>
+            <i className='bi bi-shield-lock'></i>
+            <p>Compra con seguridad</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
