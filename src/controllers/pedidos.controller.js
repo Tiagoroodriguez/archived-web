c0142c9ba43d3f6bb5c6f4a84d0d88bda44ab596
Tiago <tiagorodriguez0202@gmail.com>
2024-06-05 21:31:18 -0300

    backend - CreatePedido terminado

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -1,33 +1,116 @@
-import Envio from '../models/envio.model.js';
+import Direccion from '../models/direccion.model.js';
 import Pedido from '../models/pedido.model.js';
 
 export const createPedido = async (req, res) => {
   try {
-    const { numero_pedido, estado, informacion_envio } = req.body;
+    const {
+      // Datos de envío
+      direccion_envio,
+      numero_direccion_envio,
+      departamento_envio,
+      ciudad_envio,
+      provincia_envio,
+      codigo_postal_envio,
 
-    if (!numero_pedido || !informacion_envio) {
+      // Datos de facturación
+      direccion_facturacion,
+      numero_direccion_facturacion,
+      departamento_envio_facturacion,
+      ciudad_facturacion,
+      provincia_facturacion,
+      codigo_postal_facturacion,
+      numero_pedido,
+
+      // Datos del cliente para el Pedido
+      nombre,
+      apellido,
+      documento,
+      email,
+      telefono,
+      productos,
+    } = req.body;
+
+    if (
+      // Validacion de datos obligatorios
+      !direccion_envio ||
+      !ciudad_envio ||
+      !codigo_postal_envio ||
+      !provincia_envio ||
+      !direccion_facturacion ||
+      !ciudad_facturacion ||
+      !provincia_facturacion ||
+      !numero_pedido ||
+      !nombre ||
+      !apellido ||
+      !documento ||
+      !email ||
+      !telefono ||
+      !productos
+    ) {
       return res
         .status(400)
-        .json({ message: 'Todos los campos son obligatorios' });
+        .json({ message: 'Complete los campos obligatorios' });
     }
 
-    const newPedido = new Pedido({
+    // Crear un nuevo documento de dirección
+    const newDireccion = new Direccion({
+      direccion_envio,
+      numero_direccion_envio,
+      departamento_envio,
+      ciudad_envio,
+      provincia_envio,
+      codigo_postal_envio,
+      direccion_facturacion,
+      numero_direccion_facturacion,
+      departamento_envio_facturacion,
+      ciudad_facturacion,
+      provincia_facturacion,
+      codigo_postal_facturacion,
       numero_pedido,
-      estado,
-      informacion_envio,
+    });
+
+    const savedDireccion = await newDireccion.save();
+
+    // Crear un nuevo documento de pedido
+    const newPedido = new Pedido({
+      numero_pedido: savedDireccion.numero_pedido,
+      nombre,
+      apellido,
+      documento,
+      email,
+      telefono,
+      productos,
+      direcciones: savedDireccion._id, // ID del envío guardado
     });
 
     const savedPedido = await newPedido.save();
     res.json(savedPedido);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Algo salió mal' });
+    res.status(500).json({ message: 'Error al generar el pedido' });
   }
 };
 
 export const getPedido = async (req, res) => {
   try {
-    const pedido = await Pedido.findById(req.params.id);
+    const pedido = await Pedido.findById(req.params.id).populate(
+      'direcciones',
+      {
+        _id: 0,
+        direccion_envio: 1,
+        numero_direccion_envio: 1,
+        departamento_envio: 1,
+        ciudad_envio: 1,
+        provincia_envio: 1,
+        codigo_postal_envio: 1,
+        direccion_facturacion: 1,
+        numero_direccion_facturacion: 1,
+        departamento_envio_facturacion: 1,
+        ciudad_facturacion: 1,
+        provincia_facturacion: 1,
+        codigo_postal_facturacion: 1,
+      }
+    );
     if (!pedido)
       return res.status(404).json({ message: 'Pedido no encontrado' });
     res.json(pedido);
@@ -42,87 +125,9 @@ export const updatePedido = async (req, res) => {
       new: true,
     });
     if (!pedido)
-      return res.status(404).json({ message: 'pedido no encontrado' });
+      return res.status(404).json({ message: 'Pedido no encontrado' });
     res.json(pedido);
   } catch (error) {
-    return res.status(404).json({ message: 'pedido no encontrado' });
-  }
-};
-
-export const registrarEnvio = async (req, res) => {
-  try {
-    const {
-      nombre,
-      apellido,
-      direccion,
-      numero_direccion,
-      ciudad,
-      codigo_postal,
-      telefono,
-      email,
-      numero_pedido,
-    } = req.body;
-
-    if (
-      !nombre ||
-      !apellido ||
-      !direccion ||
-      !numero_direccion ||
-      !ciudad ||
-      !codigo_postal ||
-      !telefono ||
-      !email
-    ) {
-      return res
-        .status(400)
-        .json({ message: 'Todos los campos son obligatorios' });
-    }
-
-    const newEnvio = new Envio({
-      nombre,
-      apellido,
-      direccion,
-      numero_direccion,
-      ciudad,
-      codigo_postal,
-      telefono,
-      email,
-      numero_pedido,
-    });
-
-    const savedEnvio = await newEnvio.save();
-
-    const newPedido = new Pedido({
-      numero_pedido: 1, // Puedes cambiar esto si lo necesitas
-      informacion_envio: savedEnvio._id, // ID del envío guardado
-    });
-
-    const savedPedido = await newPedido.save();
-    res.json(savedPedido);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Algo salió mal' });
-  }
-};
-
-export const getEnvio = async (req, res) => {
-  try {
-    const envio = await Envio.findById(req.params.id);
-    if (!envio) return res.status(404).json({ message: 'Envio no encontrado' });
-    res.json(envio);
-  } catch (error) {
-    return res.status(404).json({ message: 'Envio no encontrado' });
-  }
-};
-
-export const modificarEnvio = async (req, res) => {
-  try {
-    const envio = await Envio.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!envio) return res.status(404).json({ message: 'Envio no encontrado' });
-    res.json(envio);
-  } catch (error) {
-    return res.status(404).json({ message: 'Envio no encontrado' });
+    return res.status(404).json({ message: 'Pedido no encontrado' });
   }
 };
