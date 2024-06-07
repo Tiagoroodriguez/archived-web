e52440ea6f63eb516ac723bcbe45585f8a9f83c8
Tiago <tiagorodriguez0202@gmail.com>
2024-06-07 14:17:45 -0300

    mejora de base de datos para el pedido

--- src/models/pedido.model.js
+++ src/models/pedido.model.js
@@ -5,46 +5,35 @@ const pedidoSchema = new mongoose.Schema(
     numero_pedido: {
       type: String,
       required: true,
+      unique: true,
     },
-
-    nombre: {
-      type: String,
+    cliente: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Cliente',
       required: true,
     },
-
-    apellido: {
-      type: String,
-      required: true,
-    },
-
-    documento: {
-      type: Number,
-      required: true,
-    },
-
-    email: {
-      type: String,
-      required: true,
-    },
-
-    telefono: {
-      type: String,
-      required: true,
-    },
-
     direcciones: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Direccion',
     },
-
     informacion_pago: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Pago',
     },
-
     productos: [
       {
-        type: Array,
+        producto_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Producto',
+        },
+        cantidad: {
+          type: Number,
+          required: true,
+        },
+        precio: {
+          type: Number,
+          required: true,
+        },
       },
     ],
   },
