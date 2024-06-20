b4f305f69dc09a8ccf9179235db97562a826d9aa
Tiago <tiagorodriguez0202@gmail.com>
2024-06-20 15:24:30 -0300

    contador

---
+++ client/src/components/Contador/Contador.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+export default function Contador({ targetDate }) {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    } else {
+      timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
+
+  return (
+    <div className='hero-contador'>
+      <span>{timeLeft.days} Días</span> {timeLeft.hours} Horas{' '}
+      {timeLeft.minutes} Minutos {timeLeft.seconds} Segundos
+    </div>
+  );
+}
