85c2cdaf1d406b3a1d12f6bbe937b79d11f2085e
Tiago <tiagorodriguez0202@gmail.com>
2024-07-01 21:13:35 -0300

    pedidoform

--- client/src/layout/PedidoForm/PedidoForm.jsx
+++ client/src/layout/PedidoForm/PedidoForm.jsx
@@ -2,11 +2,13 @@ import { useParams } from 'react-router-dom';
 import './PedidoForm.css';
 import { usePedido } from '../../context/PedidosContext';
 import { useEffect, useState } from 'react';
+import Select from '../../components/Select/Select';
 
 export default function PedidoForm() {
   const { id } = useParams();
   const { getPedido, pedido } = usePedido();
   const [isLoading, setIsLoading] = useState(true);
+  const [estado, setEstado] = useState('pendiente'); // Estado inicial
 
   useEffect(() => {
     const fetchPedido = async () => {
@@ -18,6 +20,7 @@ export default function PedidoForm() {
       fetchPedido();
     } else {
       setIsLoading(false);
+      setEstado(pedido.estado);
     }
   }, [id, getPedido, pedido]);
 
@@ -25,64 +28,32 @@ export default function PedidoForm() {
     return <div>Cargando...</div>;
   }
 
+  const handleEstadoChange = async (nuevoEstado) => {
+    setEstado(nuevoEstado);
+    //await updatePedido(id, { estado: nuevoEstado }); // Actualiza el pedido en la base de datos
+  };
+  const estados = [
+    { id: 1, nombre: 'pendiente' },
+    { id: 2, nombre: 'cancelado' },
+    { id: 3, nombre: 'enviado' },
+  ];
+
   return (
     <div className='pedido-form-container'>
-      <h2 className='pedido-form-header'>Detalles del Pedido</h2>
+      <h2 className='pedido-form-header'>{`Pedido número: ${pedido.numero_pedido}`}</h2>
       {pedido ? (
         <div>
           <div className='pedido-section'>
-            <h3>Información del Pedido</h3>
+            <h3>Datos del cliente</h3>
             <div className='pedido-field'>
               <p>
-                <strong>Número de Pedido:</strong> {pedido.numero_pedido}
+                <strong>DNI:</strong> {pedido.cliente_envio.documento}
               </p>
-            </div>
-          </div>
+              <p>
+                <strong>Nombre:</strong>
+                {`${pedido.cliente_envio.nombre} ${pedido.cliente_envio.apellido}`}
+              </p>
 
-          <div className='pedido-section'>
-            <h3>Cliente Facturación</h3>
-            <div className='pedido-field'>
-              <p>
-                <strong>Nombre:</strong> {pedido.cliente_facturacion.nombre}
-              </p>
-              <p>
-                <strong>Apellido:</strong> {pedido.cliente_facturacion.apellido}
-              </p>
-              <p>
-                <strong>Email:</strong> {pedido.cliente_facturacion.email}
-              </p>
-              <p>
-                <strong>Teléfono:</strong> {pedido.cliente_facturacion.telefono}
-              </p>
-            </div>
-          </div>
-
-          <div className='pedido-section'>
-            <h3>Dirección de Facturación</h3>
-            <div className='pedido-field'>
-              <p>
-                <strong>Dirección:</strong>{' '}
-                {pedido.direccion_facturacion.direccion}
-              </p>
-              <p>
-                <strong>Ciudad:</strong> {pedido.direccion_facturacion.ciudad}
-              </p>
-              <p>
-                <strong>Provincia:</strong>{' '}
-                {pedido.direccion_facturacion.provincia}
-              </p>
-            </div>
-          </div>
-
-          <div className='pedido-section'>
-            <h3>Cliente Envío</h3>
-            <div className='pedido-field'>
-              <p>
-                <strong>Nombre:</strong> {pedido.cliente_envio.nombre}
-              </p>
-              <p>
-                <strong>Apellido:</strong> {pedido.cliente_envio.apellido}
-              </p>
               <p>
                 <strong>Email:</strong> {pedido.cliente_envio.email}
               </p>
@@ -93,18 +64,28 @@ export default function PedidoForm() {
           </div>
 
           <div className='pedido-section'>
-            <h3>Dirección de Envío</h3>
-            <div className='pedido-field'>
-              <p>
-                <strong>Dirección:</strong> {pedido.direccion_envio.direccion}
-              </p>
-              <p>
-                <strong>Ciudad:</strong> {pedido.direccion_envio.ciudad}
-              </p>
-              <p>
-                <strong>Provincia:</strong> {pedido.direccion_envio.provincia}
-              </p>
-            </div>
+            {pedido.direccion_envio ? (
+              <>
+                <h3>Dirección de envío</h3>
+                <div className='pedido-field'>
+                  <p>
+                    <strong>Dirección:</strong>
+                    {`${pedido.direccion_envio.direccion} ${pedido.direccion_envio.numero}`}
+                  </p>
+                  <p>
+                    <strong>Ciudad:</strong> {pedido.direccion_envio.ciudad}
+                  </p>
+                  <p>
+                    <strong>Provincia:</strong>
+                    {pedido.direccion_envio.provincia}
+                  </p>
+                </div>
+              </>
+            ) : (
+              <>
+                <h3>Retira en el local</h3>
+              </>
+            )}
           </div>
 
           <div className='pedido-section pedido-productos'>
@@ -129,9 +110,13 @@ export default function PedidoForm() {
 
           <div className='pedido-section'>
             <h3>Estado del Pedido</h3>
-            <p className='pedido-status'>
-              <strong>Estado:</strong> {pedido.estado}
-            </p>
+            <Select
+              labelText='Estado'
+              value={estado}
+              onChange={handleEstadoChange}
+              texto='Selecciona un estado'
+              data={estados}
+            />
           </div>
         </div>
       ) : (
