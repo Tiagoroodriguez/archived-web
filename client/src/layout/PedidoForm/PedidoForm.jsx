d48a0176947c9b286fd2df4cb530732f43a75164
Tiago <tiagorodriguez0202@gmail.com>
2024-09-08 12:51:37 -0300

    Factura

--- client/src/layout/PedidoForm/PedidoForm.jsx
+++ client/src/layout/PedidoForm/PedidoForm.jsx
@@ -3,16 +3,12 @@ import { useParams } from 'react-router-dom';
 
 import './PedidoForm.css';
 import { usePedido } from '../../context/PedidosContext';
-import { useAuth } from '../../context/AuthContext';
-import PedidoFormAdmin from './PedidoFormAdmin';
 import PedidoFormUser from './PedidoFormUser';
 
 export default function PedidoForm() {
   const { id } = useParams();
   const { getPedido, pedido } = usePedido();
-  const { user } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
-  const [estado, setEstado] = useState(''); // Estado inicial
 
   useEffect(() => {
     const fetchPedido = async () => {
@@ -24,7 +20,6 @@ export default function PedidoForm() {
       fetchPedido();
     } else {
       setIsLoading(false);
-      setEstado(pedido.estado);
     }
   }, [id, getPedido, pedido]);
 
@@ -32,20 +27,5 @@ export default function PedidoForm() {
     return <div className='pedido-load'>Cargando...</div>;
   }
 
-  console.log(pedido.codigo_seguimiento);
-
-  return (
-    <>
-      {user.rol === 'admin' ? (
-        <PedidoFormAdmin
-          pedido={pedido}
-          id={id}
-          estado={estado}
-          setEstado={setEstado}
-        />
-      ) : (
-        <PedidoFormUser pedido={pedido} />
-      )}
-    </>
-  );
+  return <PedidoFormUser pedido={pedido} />;
 }
